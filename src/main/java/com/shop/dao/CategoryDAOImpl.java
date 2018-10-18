9ce9575323d8ed6ea4d4a6b29ab599fa07078b4a
Aleksandr <nezhin304@gmail.com>
2018-10-18 12:32:46 +0300

    added CategoryDAOImpl

---
+++ src/main/java/com/shop/dao/CategoryDAOImpl.java
@@ -0,0 +1,27 @@
+package com.shop.dao;
+
+import com.shop.entity.Category;
+
+import java.util.List;
+
+public class CategoryDAOImpl implements CategoryDAO {
+    @Override
+    public void create(Category category) {
+
+    }
+
+    @Override
+    public void update(Category category) {
+
+    }
+
+    @Override
+    public List<Category> getAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
